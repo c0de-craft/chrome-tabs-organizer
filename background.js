906554fc8d4ceb4c46242ca87ddb5ccbf9df50cd
c0de-craft <140589904+c0de-craft@users.noreply.github.com>
2023-07-26 09:33:07 +0200

    feat(): fix empty tab
    feat(): add async await

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
   let domain;
-  if (tab.url === 'chrome://newtab/') {
-    domain = 'empty';
+  let isNewTab = false;
+
+  if (!tab.url || tab.url.startsWith('chrome://')) {
+    // Handle new tab (empty page) case separately
+    isNewTab = true;
   } else {
     try {
       domain = new URL(tab.url).hostname;
@@ -11,34 +14,44 @@ chrome.action.onClicked.addListener((tab) => {
     }
   }
 
-  chrome.tabs.query({}).then((tabs) => {
-    let domainTabs = tabs.filter((tab) => {
-      if (domain === 'empty') {
-        return tab.url === 'chrome://newtab/';
-      } else {
-        let url;
-        if (!tab.url || tab.url.startsWith('chrome://')) {
-          // Not a valid URL or a special Chrome URL, skip this tab
-          return false;
-        }
-        try {
-          url = new URL(tab.url);
-        } catch (_) {
-          // Not a valid URL, skip this tab
-          return false;
-        }
-        return url.hostname.includes(domain);
+  const tabs = await chrome.tabs.query({});
+  let domainTabs = tabs.filter((tab) => {
+    if (isNewTab) {
+      // Collect all new tabs (empty pages)
+      return !tab.url || tab.url.startsWith('chrome://');
+    } else {
+      // Collect all tabs with the same domain
+      let url;
+      if (!tab.url || tab.url.startsWith('chrome://')) {
+        // Not a valid URL or a special Chrome URL, skip this tab
+        return false;
       }
-    });
-
-    if (domainTabs.length > 0) {
-      let tabIds = domainTabs.map((tab) => tab.id);
-      chrome.windows.create({}).then((window) => {
-        chrome.tabs.move(tabIds, {
-          windowId: window.id,
-          index: -1
-        });
-      });
+      try {
+        url = new URL(tab.url);
+      } catch (_) {
+        // Not a valid URL, skip this tab
+        return false;
+      }
+      return url.hostname.includes(domain);
     }
   });
+
+  if (domainTabs.length > 0) {
+    let tabIds = domainTabs.map((tab) => tab.id);
+    const newWindow = await chrome.windows.create({});
+    // Move the first tab to the new window
+    await chrome.tabs.move(tabIds[0], {
+      windowId: newWindow.id,
+      index: -1
+    });
+    // Remove the default tab in the new window
+    await chrome.tabs.remove(newWindow.tabs[0].id);
+    // If there are more tabs, move them to the new window
+    if (tabIds.length > 1) {
+      await chrome.tabs.move(tabIds.slice(1), {
+        windowId: newWindow.id,
+        index: -1
+      });
+    }
+  }
 });
